96bd5f86ed9427151551648db254fdc3a8af7042
shazheyi <48951206+shazheyi@users.noreply.github.com>
2020-04-05 09:31:46 +0800

    Add files via upload

---
+++ Lab 4 Mujava/BackPackTest.java
@@ -0,0 +1,56 @@
+﻿import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BackPackTest {
+
+	private BackPack backpack;
+
+	 
+
+    @Before
+
+    public void setUp() throws Exception {
+
+        // setUp()用于测试前的初始化
+
+        backpack = new BackPack();
+
+    }
+
+    @Test
+
+    public void test2() {
+
+    int m = 10;
+
+        int n = 3;
+
+    int w[] = {3, 4, 5};
+
+        int p[] = {4, 5, 6};
+
+        int a[][] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+            {0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4},
+
+            {0, 0, 0, 4, 5, 5, 5, 9, 9, 9, 9},
+
+            {0, 0, 0, 4, 5, 6, 6, 9, 10, 11, 11}};
+
+        Assert.assertArrayEquals(a,backpack.BackPack_Solution(m, n, w, p));     
+
+    }
+
+    @After
+
+    public void tearDown() throws Exception {
+
+        backpack = null;
+
+    }
+
+}
---
+++ Lab 4 Mujava/BubbleSortTest.java
@@ -0,0 +1,33 @@
+﻿import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BubbleSortTest {
+	 private BubbleSort bubble;
+
+	    @Before
+	    public void setUp() throws Exception {
+	        bubble = new BubbleSort();
+
+	    }
+
+	    @Test
+	    public void test1() {
+	        int a[] = new int[]{1,6,2,2,5};
+	        int b[]=new int[]{2,2,2,1,2,13};
+	        int a1[] = new int[]{1,2,2,5,6};
+	        int b1[]=new int[]{1,2,2,2,2,13};
+	        assertEquals(Arrays.toString(a1), Arrays.toString(bubble.BubbleSort(a)));
+	        assertEquals(Arrays.toString(b1), Arrays.toString(bubble.BubbleSort(b)));     
+
+	    }
+
+	    @After
+	    public void tearDown() throws Exception {
+	        bubble = null;
+	    }
+
+}
